e298091fba8059ca3fc8ab7ac15c0e9d98815485
Vasily_Shaytan <svshayt@gmail.com>
2024-04-09 13:30:49 +0300

    Создать демо проект JavaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module com.svshayt.javafxdemo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires kotlin.stdlib;
+
+    opens com.svshayt.javafxdemo to javafx.fxml;
+    exports com.svshayt.javafxdemo;
+}
